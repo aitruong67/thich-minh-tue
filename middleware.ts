d1db1598db90d96d44211667bff6470faae20e02
aitruong67 <aitruongse@gmail.com>
2026-05-17 21:36:43 -0700

    fix: exclude /admin from next-intl middleware to prevent locale redirect

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { routing } from './i18n/routing'
 export default createMiddleware(routing)
 
 export const config = {
-  matcher: ['/((?!api|studio|structure|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|admin|studio|structure|_next|_vercel|.*\\..*).*)'],
 }
